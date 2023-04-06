86f689c0b838684d6402c4c6a408576ea356865b
Nima Babak <80975120+nimababak@users.noreply.github.com>
2023-04-05 23:33:32 -0400

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    render :file => 'public/index.html.erb'
+#    render :file => 'public/index.html'
+    render :file => '/app/views/welcome/index.html.erb' 
   end
 end
