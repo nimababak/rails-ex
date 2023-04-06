00953af915f993bc92180175710786fb28de527c
Nima Babak <80975120+nimababak@users.noreply.github.com>
2023-04-06 00:25:55 -0400

    Update welcome_controller_test.rb

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get articles" do
-    get welcome_articles_url
+    get welcome_index_url
     assert_response :success
   end
 
